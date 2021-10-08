bed8954019e5a713f1e525b5009e8bf8ebc57c81
Dandariene <dandariene@hotmail.com>
2021-10-08 20:43:04 -0300

    Exercicios de revisão de javascript até a 17B

--- quinzena3/revisao-js/lista-exercicios-revisao-js/exercicios.js
+++ quinzena3/revisao-js/lista-exercicios-revisao-js/exercicios.js
@@ -1,7 +1,6 @@
 // EXERCÍCIO 01
 function inverteArray(array) {
   const arrayInvertido = array.map((item, indice, array) => array[array.length - indice - 1])
-
   return arrayInvertido
 }
 
@@ -9,29 +8,24 @@ function inverteArray(array) {
 function retornaNumerosParesElevadosADois(array) {
   const numerosPares = array.filter(item => item % 2 === 0).map(item => item * item)
   return numerosPares
-
 }
 
 // EXERCÍCIO 03
 function retornaNumerosPares(array) {
-
   const numerosPares = array.filter(item => item % 2 === 0)
   return numerosPares
-
 }
 
 // EXERCÍCIO 04
 function retornaMaiorNumero(array) {
   const maior = Math.max(...array)
   return maior
-
 }
 
 // EXERCÍCIO 05
 function retornaQuantidadeElementos(array) {
   const qtdElementos = array.length
   return qtdElementos
-
 }
 
 // EXERCÍCIO 06
@@ -55,19 +49,15 @@ function retornaNNumerosPares(n) {
   for (let i = 0; i < n; i++) {
     numerosPares.push(i * 2)
   }
-
   return numerosPares
-
 }
 
 // EXERCÍCIO 08
 function checaTriangulo(a, b, c) {
   if (a === b && b === c) {
     return 'Equilátero'
-
   } else if (a === b || b === c) {
     return 'Isósceles'
-
   } else {
     return 'Escaleno'
   }
@@ -80,31 +70,35 @@ function comparaDoisNumeros(num1, num2) {
     maiorDivisivelPorMenor: 0,
     diferenca: 0
   }
-
   const verificaSeDivide = (num1, num2) => {
     return num1 % num2 === 0
   }
-
   if (num1 < num2) {
     comparador.maiorNumero = num2
     comparador.maiorDivisivelPorMenor = verificaSeDivide(num2, num1)
     comparador.diferenca = num2 - num1
-
     return comparador
-
   } else {
     comparador.maiorNumero = num1
     comparador.maiorDivisivelPorMenor = verificaSeDivide(num1, num2)
     comparador.diferenca = num1 - num2
-
-
     return comparador
   }
 }
 
 // EXERCÍCIO 10
 function segundoMaiorEMenor(array) {
-
+  novoArray = []
+  for (item of array) {
+    let contador = 0
+    for (itemcomparado of array) {
+      if (item > itemcomparado) {
+        contador = contador + 1
+      }
+    }
+    novoArray[contador] = item
+  }
+  return [novoArray[novoArray.length - 2], novoArray[1]]
 }
 
 // EXERCÍCIO 11
@@ -131,46 +125,90 @@ function filmeFavorito() {
     atores: ['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci'],
   }
   return (filmeFavoritoAstrodev)
-
 }
 
 // EXERCÍCIO 13
 function imprimeChamada() {
   // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+  let filme = {
+    nome: "O Diabo Veste Prada",
+    ano: 2006,
+    diretor: "David Frankel",
+    atores: ['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci'],
+  }
+  return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
 }
 
 // EXERCÍCIO 14
 function criaRetangulo(lado1, lado2) {
-
+  largura = lado1
+  altura = lado2
+  perimetro = 2 * (lado1 + lado2)
+  area = lado1 * lado2
+  const objeto = {
+    largura: largura,
+    altura: altura,
+    perimetro: perimetro,
+    area: area
+  }
+  return (objeto)
 }
 
 // EXERCÍCIO 15
 function anonimizaPessoa(pessoa) {
-
+  const pessoaAnonima = {
+    ...pessoa,
+    nome: "ANÔNIMO"
+  }
+  return (pessoaAnonima)
 }
 
-// EXERCÍCIO 16A
+//EXERCÍCIO 16A
 function maioresDe18(arrayDePessoas) {
-
+  const  maior18 = arrayDePessoas.filter((item) => {
+    return item.idade >= 18
+  })
+  return maior18
+  
 }
 
 // EXERCÍCIO 16B
 function menoresDe18(arrayDePessoas) {
+  const  menor18 = arrayDePessoas.filter((item) => {
+    return item.idade < 18
+  })
+  return menor18
 
 }
 
 // EXERCÍCIO 17A
 function multiplicaArrayPor2(array) {
-
+  const  multPor2 = array.map((item) => {
+    return item * 2
+  })
+  return multPor2
 }
 
 // EXERCÍCIO 17B
 function multiplicaArrayPor2S(array) {
-
+  const multPor2Str = array.map((item) => {
+    item = item * 2
+    return item.toString()
+  }) 
+  return multPor2Str
 }
 
 // EXERCÍCIO 17C
 function verificaParidade(array) {
+  const imparPar = array.map((item) => {
+    if (item % 2 === 0){
+      item = `${item.toString()} é par`
+    }else{
+      item = `${item.toString()} é impar`
+    }
+   return item
+  })
+  return(imparPar)
 
 }
 
